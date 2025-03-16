60f6f5dac6772f8608fb6bc16778207a25582c71
Bruno Leonardo <135679874+brunols7@users.noreply.github.com>
2025-03-16 03:11:14 -0300

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -23,13 +23,28 @@ class MainApp extends StatelessWidget {
         appBar: AppBar(
           centerTitle: true,
           title: Image.asset('assets/images/f1-logo.png', height: 40),
-          backgroundColor: Color(0xFFf8f4f0),
           elevation: 0.5,
+          backgroundColor: Color(0xFFf8f4f0),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.info_outlined, color: Color(0xFFFA1F00), size: 24),
+              onPressed: ((){}),
+            )
+          ],
         ),
         body: Container(
           margin: EdgeInsets.all(20),
           child: Column(
             children: [
+              Text(
+                'TEAMS',
+                style: TextStyle(
+                  fontFamily: 'f1',
+                  fontSize: 24,
+                  color: Colors.black,
+                )
+              ),
+              SizedBox(height: 10),
               Expanded(
                 child: ListView.separated(
                   physics: BouncingScrollPhysics(),
